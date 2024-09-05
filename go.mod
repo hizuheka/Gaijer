0e46197b058f7d153ec99539a3e77cbd6bbef5c3
hizuheka <44028436+hizuheka@users.noreply.github.com>
2024-09-05 16:48:36 +0900

    .

--- go.mod
+++ go.mod
@@ -4,6 +4,5 @@ go 1.22.3
 
 require (
 	github.com/google/subcommands v1.2.0
-	github.com/mattn/go-shellwords v1.0.12
 	golang.org/x/text v0.6.0
 )
--- main.go
+++ main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"context"
 	"flag"
 	"fmt"
-	"io"
 	"log/slog"
 	"os"
 
 	"Gaijer/cmd/find"
-	unlsql "Gaijer/cmd/sql"
 
 	"github.com/google/subcommands"
 )
+
+var version string
 
 func main() {
 	subcommands.Register(subcommands.HelpCommand(), "")
 	subcommands.Register(subcommands.FlagsCommand(), "")
 	subcommands.Register(subcommands.CommandsCommand(), "")
 	subcommands.Register(&find.FindCmd{}, "")
-	subcommands.Register(&unlsql.UnlsqlCmd{}, "")
 
 	isDebug := flag.Bool("d", false, "debugログを出力")
+	isVersion := flag.Bool("v", false, "バージョンを出力")
 	flag.Parse()
+
+	// バージョン出力
+	if *isVersion {
+		fmt.Printf("Gaijer.exe version %s\n", version)
+		os.Exit(int(subcommands.ExitSuccess))
+	}
 
 	// ログレベルの設定
 	switch {
@@ -36,62 +40,4 @@ func main() {
 	ctx := context.Background()
 
 	os.Exit(int(subcommands.Execute(ctx)))
-
-	// fpIn, err := os.Open("input.txt")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer fpIn.Close()
-
-	// for {
-	// 	reader := bufio.NewReaderSize(fpIn, 1000*10)
-	// 	chunk, err := readFileByChunk(reader, 1000)
-	// 	if err != nil {
-	// 		break
-	// 	}
-	// 	// todo 外字ハッシュリストの、外字構造体の使用有無がfalseのものを対象に、繰り返し処理する
-	// 	for _, v := range gaijiList {
-	// 		if !v.used {
-	// 			// todo 検索対象の100バイトの中に、外字が含まれるかを調べる
-	// 			// * 読み込んだバイト配列の中に、外字が含まれるかを調べる。
-	// 			// * 読み込んだバイト配列の2バイト文字はリトルエンディアンとなっているため、
-	// 			// * 外字はリトルエンディアンに変換する。
-	// 			b := [2]byte{}
-	// 			copy(b[:], []byte(string(v.moji)))
-	// 			if contains(chunk, swap(b)) {
-	// 				// * 外字が含まれた場合は、外字構造体の使用有無に、trueをセットする
-	// 				v.used = true
-	// 			}
-	// 		}
-	// 	}
-	// }
-
-	// for n, v := range gaijiList {
-	// 	fmt.Printf("%v:%s-%v", n, string(v.moji), v.used)
-	// }
-}
-
-// readFileByChunkは、指定されたファイルを指定されたサイズ chunkSize で分割して読み取ります
-func readFileByChunk(r *bufio.Reader, chunkSize int) ([]byte, error) {
-	buffer := make([]byte, chunkSize)
-	n, err := r.Read(buffer)
-	if err != nil {
-		if err == io.EOF {
-			return nil, io.EOF
-		}
-		panic(err)
-	}
-	fmt.Println(string(buffer[:n]))
-	return buffer[:n], nil
-}
-
-// * swapはバイトの配列を受け取り、配列の要素の順番を反転します。
-func swap(a [2]byte) [2]byte {
-	a[0], a[1] = a[1], a[0]
-	return a
-}
-
-// * containsはb1内にb2があるかどうかを判断します
-func contains(b1 []byte, b2 [2]byte) bool {
-	return bytes.Contains(b1, b2[:])
 }
